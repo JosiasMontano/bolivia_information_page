4e2d9c083abb512d47ca9dcf8cf403f49caa19c6
josias <josisamu10@gmail.com>
2025-08-18 21:07:11 -0400

    jsx const added

--- app.jsx
+++ app.jsx
@@ -23,7 +23,7 @@ const boliviaData = {
         principales_socios_comerciales: "Brasil, Argentina, China, Estados Unidos" 
     },
     departamentos: {
-         cochabamba: {
+        cochabamba: {
             nombre: "Cochabamba",
             imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-cochabamba-0116.jpg",
             video: "https://www.youtube.com/embed/XjyawCcHhQc",
@@ -43,42 +43,129 @@ const boliviaData = {
             nombre: "Tarija",
             imagen: "https://i.pinimg.com/originals/a8/ea/68/a8ea68616b039efd40d13459aaf1897b.jpg",
             video: "https://www.youtube.com/embed/xyzeiUqUdqk",
-            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-64.5!3d-21.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Tarija!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo"
-            
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-64.5!3d-21.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Tarija!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "37,623 km²",
+            poblacion: "600,000 ",
+            altura: "400-2,500 msnm",
+            densidad: "16 hab/km²",
+            idiomas: "Español, guaraní (Chaco)",
+            gentilicio: "Tarijeño/a",
+            economia: "Vitivinicultura, agricultura (maíz, papa), hidrocarburos",
+            clima: "Templado-cálido (15°C a 28°C), inviernos suaves",
+            festividades: "Fiesta de San Roque, Vendimia tarijeña",
+            gastronomia: "Saice, Ranga ranga, Chancho a la cruz, Vinos."
         },
         santacruz: {
             nombre: "Santa Cruz",
             imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-santa-cruz-0116.jpg",
             video: "https://www.youtube.com/embed/V871IYGsa-s?si=5IZbZicl8hXQH4fY",
-            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-62.5!3d-17.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93fbc3d6c1e2a0a5%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Santa%20Cruz!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo"
-            
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-62.5!3d-17.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93fbc3d6c1e2a0a5%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Santa%20Cruz!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "370,621 km²",
+            poblacion: "3,500,000 hab",
+            altura: "200-1500 msnm",
+            densidad: "9 hab/km²",
+            idiomas: "Español, guaraní, chiquitano y mojeño",
+            gentilicio: "Cruceño/a",
+            economia: "Agroindustria (soja, girasol), hidrocarburos, comercio, servicios",
+            clima: "Tropical (22°C a 35°C), con humedad alta en verano",
+            festividades: "Fiesta de la Virgen de Cotoca, Carnaval Cruceño",
+            gastronomia: "Majadito, Sonso, Locro, Empanadas de arroz."
         },
         lapaz: {
             nombre: "La Paz",
             imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-la-paz-0116.jpg",
             video: "https://www.youtube.com/embed/AyOtlWnb0As?si=UNrThAPMdLsvKMhj",
-            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-69.5!3d-16.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20La%20Paz!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo"
-            
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-69.5!3d-16.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20La%20Paz!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "133,985 km²",
+            poblacion: "2,900,000 hab",
+            altura: "3,000 a 4,150 msnm",
+            densidad: "22 hab/km²",
+            idiomas: "Español, aymara (mayoritario), quechua",
+            gentilicio: "Paceño/a",
+            economia: "Servicios , Agricultura , Minería e Hidroeléctricas",
+            clima: "Frio (11°C a 25°C promedio)",
+            festividades: "Alasitas, Fiesta del Gran Poder",
+            gastronomia: "Plato Paceño, Chairo, Thimpu , Café de los Yungas."
         },
         oruro: {
             nombre: "Oruro",
             imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-oruro-0116.jpg",
             video: "https://www.youtube.com/embed/0A3Aq-5hp8A?si=xAKrCj8LZpnHMjXG",
-            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-68.5!3d-18.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Oruro!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo"
-            
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-68.5!3d-18.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Oruro!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "53,588 km²",
+            poblacion: "550,000 hab",
+            altura: "3,700-4,000 msnm",
+           densidad: "10 hab/km²",
+            idiomas: "Español, Quechua y Aymara",
+            gentilicio: "Oureño/a",
+            economia: "Minería (estaño, plata), comercio, turismo",
+            clima: "Frío y seco (5°C a 15°C)",
+            festividades: "Carnaval de Oruro, Fiesta del Socavón",
+            gastronomia: "Charquekán, Rostro asado, Api con pastel"
         },
         potosi: {
             nombre: "Potosí",
             imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-potosi-0116.jpg",
             video: "https://www.youtube.com/embed/VbRV4WNBzKI?si=ucExX-8kDjsTwULr",
-            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-67.5!3d-20.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Potos%C3%AD!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo"
-           
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-67.5!3d-20.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Potos%C3%AD!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "118,218 km²",
+            poblacion: "900,000 hab",
+            altura: "4,000 msnm (promedio)",
+            densidad: "7 hab/km²",
+            idiomas: "Español, Quechua",
+            gentilicio: "Potosino/a",
+            economia: "Minería (plata, zinc, plomo), turismo",
+            clima: "Frío y seco (0°C a 15°C)",
+            festividades: "Fiesta de Ch'utillos, Carnaval Minero",
+            gastronomia: "K'ala phurka, Ají de arvejas, Asado potosino"
         },
-        pando: {  
+        pando: {
+            nombre: "Pando",
+            imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-pando-0116.jpg",
+            video: "https://www.youtube.com/embed/CyP8TRkOPZQ?si=q4NARG1st4ZikCc9",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-67.5!3d-11.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Pando!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "63,827 km²",
+            poblacion: "154,000 hab",
+            altura: "180 msnm (promedio)",
+            densidad: "2 hab/km²",
+            idiomas: "Español, portugués (regional)",
+            gentilicio: "Pandino/a",
+            economia: "Castaña, madera, turismo ecológico",
+            clima: "Tropical húmedo (25°C a 35°C)",
+            festividades: "Feria de la Castaña, Aniversario de Pando",
+            gastronomia: "Majao, Pacumutu, Asado de jochi"
         },
         beni: {
+            nombre: "Beni",
+            imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-beni-0116.jpg",
+            video: "https://www.youtube.com/embed/vu9Xz9HSboo?si=0UyVZD6lJHFPZJpO",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-65.5!3d-14.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20del%20Beni!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "213,564 km²",
+            poblacion: "480,000 hab",
+            altura: "150 msnm",
+            densidad: "2 hab/km²",
+            idiomas: "Español, mojeño, trinitario (lenguas indígenas)",
+            gentilicio: "Beniano/a",
+            economia: "Ganadería, agricultura (arroz, maíz), turismo ecológico",
+            clima: "Tropical húmedo (23°C a 35°C)",
+            festividades: "Fiesta del Santo Patrono de Trinidad, Feria Exposición",
+           gastronomia: "Masaco, Keperi, Majao, Sopa de maní"
         },
         sucre: {
+            nombre: "Sucre",
+            imagen: "https://www.educa.com.bo/sites/default/files/mapa-politico-chuquisaca-0116.jpg",
+            video: "https://www.youtube.com/embed/PP-mRDeQCrs?si=JF76ZkvOZsFmZreF",
+            mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827296.714534921!2d-65.5!3d-20.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915edf0a04a5a0ab%3A0x4e36fe51dfc7e974!2sDepartamento%20de%20Chuquisaca!5e0!3m2!1ses!2sbo!4v1710000000000!5m2!1ses!2sbo",
+            extension: "51,524 km²",
+            poblacion: "637,000 hab",
+            altura: "50-3500 msnm",
+            densidad: "12 hab/km²",
+            idiomas: "Español, quechua, guaraní (Chaco)",
+            gentilicio: "Chuquisaqueño/a",
+            economia: "Agricultura (maíz, trigo), turismo histórico, servicios educativos",
+            clima: "Variado (templado en valles, cálido en el Chaco)",
+            festividades: "Pujllay, Festival de la Cultura",
+            gastronomia: "Chorizos chuquisaqueños, Sopa de maní, Ají de papalisa"
         }
     }
 };
